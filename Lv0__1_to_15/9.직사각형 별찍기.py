cba49ca5ca83744a926fa78f1fae81a88ba7bffc
itwebMJ <rlaalwn61@naver.com>
2021-07-22 21:18:57 +0900

    practice algorithm(직사각형 별찍기)

---
+++ Lv0__1_to_15/9.직사각형 별찍기.py
@@ -0,0 +1,10 @@
+'''
+직사각형 별찍기
+문제 설명
+이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+a, b = map(int, input().strip().split(' '))
+'''
+n, m = map(int, input().strip().split(' '))     #두 수를 입력받아 출력
+for i in range(0, m):
+    print('*'*n)
